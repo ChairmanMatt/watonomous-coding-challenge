25588fa5b016912ae166dfc2d74d7afd73beaa71
Matt Lu <CMS-177116@hotmail.com>
2018-01-09 21:50:11 -0500

    Comments added

--- build.sh
+++ build.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
+#terminate on error
 set -e
+
+#make
 make
 
+#linting
 cpplint headers/*.h
 cpplint *.cpp
 
+#run executable - note, -e tag means will not run unless make and linting succeed
 ./runner
 
 exit 0
